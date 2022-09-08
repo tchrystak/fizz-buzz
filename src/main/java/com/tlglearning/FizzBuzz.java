6940bd9a1986cc6bea9d5c15d7ba2dec0e76351e
tchrystak <chrystaktrey@gmail.com>
2022-09-08 11:02:43 -0700

    FizzBuzz Enums

--- src/main/java/com/tlglearning/FizzBuzz.java
+++ src/main/java/com/tlglearning/FizzBuzz.java
@@ -1,20 +1,19 @@
 package com.tlglearning;
 
-public class FizzBuzz {
+import java.util.EnumSet;
+import java.util.Set;
 
-  public static final String FIZZ_RESULT = "fizz";
-  public static final String BUZZ_RESULT = "buzz";
+public enum FizzBuzz {
 
-  public static String valueOf(int num) {
-    String result = "";
+  FIZZ, BUZZ;
+
+  public static Set<FizzBuzz> valueOf(int num) {
+    Set<FizzBuzz> result = EnumSet.noneOf(FizzBuzz.class);
     if (num % 3 == 0) {
-      result += FIZZ_RESULT;
+      result.add(FIZZ);
     }
     if (num % 5 == 0) {
-      result += BUZZ_RESULT;
-    }
-    if (result.isEmpty()) {
-      result = String.valueOf(num);
+      result.add(BUZZ);
     }
     return result;
   }
--- src/main/java/com/tlglearning/Main.java
+++ src/main/java/com/tlglearning/Main.java
@@ -1,11 +1,30 @@
 package com.tlglearning;
 
+import java.util.Set;
+
 public class Main {
 
-  public static void main(String[] args) {
-    for (int i = 0; i <= 100; i++) {
-      System.out.println(FizzBuzz.valueOf(i));
-    }
-  }
+  /*while loop */
 
+//  public static void main(String[] args) {
+//    int i = 1;
+//    while ( i <= 100) { // while loop
+//      String value =(FizzBuzz.valueOf(i));
+//      System.out.println(value);
+//      if (value.equals("fizzbuzz")) {
+//        break;
+//      }
+//      i++;
+//    }
+//  }
+
+  // do while
+  public static void main(String[] args) {
+    int i = 1;
+    do {
+      Set<FizzBuzz> value =(FizzBuzz.valueOf(i));
+      System.out.printf("%d:%s%n", i, value);
+       i++;
+    } while (i <= 100);
+  }
 }
