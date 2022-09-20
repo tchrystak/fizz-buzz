b22bd9e5adf1fdb67571f7d97a63e15dad377d91
tchrystak <chrystaktrey@gmail.com>
2022-09-20 11:02:52 -0700

    FizzBuzz updated Enums

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,11 +2,13 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
   @Test
   void valueOf() {
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
   }
 }
